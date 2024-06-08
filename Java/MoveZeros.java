36f8f86d95ebcffd94a690973572b5bc40d8cfb1
avish <avishusa45@gmail.com>
2024-06-07 20:33:09 -0500

    283-MoveZeros

---
+++ Java/MoveZeros.java
@@ -0,0 +1,39 @@
+// 283->Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int i=-1;
+        
+        for(int n=0;n<arr.length;n++){
+            if(arr[n]==0){
+                i=n;
+                break;
+            }
+        }
+        if(i==-1){
+            return;
+        }
+        
+        for(int j = i+1;j<arr.length;j++){
+            if(arr[j]!=0){
+                arr[i]=arr[j];
+                arr[j]=0;
+                i++;
+            }
+        }
+    }
+}
